04fb58c16ccb9adbd0ab0a92b9470becbedab55d
BatsCrow <twilliamsa776@gmail.com>
2026-04-25 17:26:40 -0400

    Physical arm constraints

--- firmware/RIOO Firmware/include/constants.h
+++ firmware/RIOO Firmware/include/constants.h
@@ -10,11 +10,19 @@
 #define TRIG_PIN 5
 #define ECHO_PIN 12
 
-// SERVOS
+// === SERVOS === //
 
 #define BASE_SERVO_PIN 13
 #define SHOULDER_SERVO_PIN 14
 #define ELBOW_SERVO_PIN 18
 #define GRIPPER_SERVO_PIN 19
+
+// === ARM MEASUREMENTS & CONSTRAINTS === //
+
+const float SHOULDER_LENGTH = 127.0;
+const float FOREARM_LENGTH = 101.6;
+
+const float MIN_JOINT = 0.0;
+const float MAX_JOINT = 90.0;
 
 #endif // CONSTANTS_H
