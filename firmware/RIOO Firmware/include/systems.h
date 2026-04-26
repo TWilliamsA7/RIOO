739cedb040706b742111f753f2ac6fec9d80b4a9
BatsCrow <twilliamsa776@gmail.com>
2026-04-26 01:43:29 -0400

    Non-blocking ultrasonic sensors

--- firmware/RIOO Firmware/include/systems.h
+++ firmware/RIOO Firmware/include/systems.h
@@ -3,6 +3,8 @@
 
 #include "Adafruit_VL6180X.h"
 #include "ESP32Servo.h"
+#include "NewPing.h"
+#include "constants.h"
 
 // === SERVOS === //
 
@@ -14,5 +16,11 @@ extern Servo GripperServo;
 // === TOF SENSORS === //
 
 extern Adafruit_VL6180X gripperTOF;
+
+// === ULTRASONIC SENSORS === //
+
+extern NewPing leftSonar;
+extern NewPing rightSonar;
+extern NewPing sonars[ULTRASONIC_NUM];
 
 #endif // SYSTEMS_H
