a9debc5fb6f83ec82bf32dd67f8cf3ea673f8952
BatsCrow <twilliamsa776@gmail.com>
2026-04-26 12:35:19 -0400

    UART implementation for vision

--- firmware/RIOO Firmware/src/rioo.cpp
+++ firmware/RIOO Firmware/src/rioo.cpp
@@ -30,7 +30,7 @@ void runRIOO() {
         heartbeatLED();
 
         // Check collision sensors
-        updateCollisionSensors();
+        //updateCollisionSensors();
 
         // Iterate over distances
         for (int i = 0; i < ULTRASONIC_NUM; i++) {
@@ -43,12 +43,12 @@ void runRIOO() {
 
         toggleGrip(gazeCommand.grab);
 
-        updateDebugScreen(gazeCommand.xPos, gazeCommand.yPos, 0, collisionSensorDistances[0], collisionSensorDistances[1]);
+        // updateDebugScreen(gazeCommand.xPos, gazeCommand.zPos, 0, collisionSensorDistances[0], collisionSensorDistances[1]);
 
         // Only move ARM when the claw is not moving
         if (clawActionDone) {
             // Compute new target based on UART data
-            Point target = computeTargetPoint(gazeCommand.xPos, gazeCommand.yPos);
+            Point target = computeTargetPoint(gazeCommand.xPos, gazeCommand.zPos);
         
             // Use IK to calculate expected angles of joints
             JointAngles ja = calculateIK(target);
@@ -62,7 +62,7 @@ void runRIOO() {
 }
 
 float prevTargetX = 0.0f;
-float prevTargetY = 0.0f;
+float prevTargetZ = 0.0f;
 
 float mapFloat(float x, float in_min, float in_max, float out_min, float out_max) {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
@@ -97,26 +97,24 @@ void calculateEvasiveManeuver(JointAngles &ja) {
 }
 
 
-Point computeTargetPoint(float rawX, float rawY) {
-    float targetX = mapFloat(rawX, -1.0, 1.0, min_working_x, max_working_x);
-    float targetY = mapFloat(rawY, -1.0, 1.0, min_working_y, max_working_y);
-
+Point computeTargetPoint(float rawX, float rawZ) {
     static bool firstRun = true;
     if (firstRun) {
-        prevTargetX = targetX;
-        prevTargetY = targetY;
+        prevTargetX = rawX;
+        prevTargetZ = rawZ;
         firstRun = false;
     }
 
-    targetX = (PLANAR_ALPHA * targetX) + ((1.0 - PLANAR_ALPHA) * prevTargetX);
-    targetY = (PLANAR_ALPHA * targetY) + ((1.0 - PLANAR_ALPHA) * prevTargetY);
+    float targetX = (PLANAR_ALPHA * rawX) + ((1.0 - PLANAR_ALPHA) * prevTargetX);
+    float targetZ = (PLANAR_ALPHA * rawZ) + ((1.0 - PLANAR_ALPHA) * prevTargetZ);
 
     Point newTarget;
     newTarget.x = targetX;
-    newTarget.y = targetY;
+    newTarget.y = 100;
     prevTargetX = targetX;
-    prevTargetY = targetY;
-
-    computeTargetZ(newTarget);
+    prevTargetZ = targetZ;
+    newTarget.z = targetZ;
+    //computeTargetZ(newTarget);
+     
     return newTarget;
 }
